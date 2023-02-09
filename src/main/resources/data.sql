1a2548fbb2b5252e0881a9e9269ad050776fef79
Hyun Woo <hwsa10041@gmail.com>
2023-02-09 13:50:53 +0900

    [BUILD] Authroity 계정 타입 세팅
    1. 임대인
    2. 공인 중개사
    3. 임차인

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO authority (authority_name) VALUES ('ROLE_LESSOR');
+INSERT INTO authority (authority_name) VALUES ('ROLE_REALTOR');
+INSERT INTO authority (authority_name) VALUES ('ROLE_LESSEE');
