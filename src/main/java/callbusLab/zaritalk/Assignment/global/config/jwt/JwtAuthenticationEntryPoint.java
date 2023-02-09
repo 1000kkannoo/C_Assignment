ffa09da1e29cea1383463d56a1bcdb1fa7d75459
Hyun Woo <hwsa10041@gmail.com>
2023-02-09 15:34:37 +0900

    [FEAT] JWT 세팅

---
+++ src/main/java/callbusLab/zaritalk/Assignment/global/config/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package callbusLab.zaritalk.Assignment.global.config.jwt;
+
+import callbusLab.zaritalk.Assignment.global.config.exception.CustomErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static callbusLab.zaritalk.Assignment.global.config.exception.CustomErrorCode.Authentication_Entry_Point;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        //Content-type : application/json;charset=utf-8
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        CustomErrorResponse error = new CustomErrorResponse();
+        error.setStatus(Authentication_Entry_Point);
+        error.setStatusMessage("토큰이 NULL 이거나 잘못된 AccessToken 입니다.");
+
+        // {"username":"loop-study", "age":20}
+        String result = objectMapper.writeValueAsString(error);
+        response.getWriter().write(result);
+    }
+}
---
+++ src/main/java/callbusLab/zaritalk/Assignment/global/config/jwt/JwtUserService.java
@@ -0,0 +1,45 @@
+package callbusLab.zaritalk.Assignment.global.config.jwt;
+
+import callbusLab.zaritalk.Assignment.domain.user.entity.User;
+import callbusLab.zaritalk.Assignment.domain.user.repository.UserRepository;
+import callbusLab.zaritalk.Assignment.global.config.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static callbusLab.zaritalk.Assignment.global.config.exception.CustomErrorCode.LOGIN_FALSE;
+
+@Service
+@Component("userDetailsService")
+@RequiredArgsConstructor
+public class JwtUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String email) {
+        return userRepository.findOneWithAuthoritiesByEmail(email)
+                .map(this::createUser)
+                .orElseThrow(() -> new CustomException(LOGIN_FALSE));
+    }
+
+    private org.springframework.security.core.userdetails.User createUser(User user) {
+
+        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPw(),
+                grantedAuthorities
+        );
+    }
+}
