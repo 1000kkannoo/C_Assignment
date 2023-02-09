a186da1103a855fae5c64d7574c80bb8233b1855
Hyun Woo <hwsa10041@gmail.com>
2023-02-09 15:37:23 +0900

    [FEAT] Repository 쿼리 메서드 생성

--- src/main/java/callbusLab/zaritalk/Assignment/domain/user/repository/UserRepository.java
+++ src/main/java/callbusLab/zaritalk/Assignment/domain/user/repository/UserRepository.java
@@ -4,6 +4,10 @@ import callbusLab.zaritalk.Assignment.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findOneWithAuthoritiesByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
