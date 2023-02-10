6cea4f437c8fe6e8ef540957cd56a9c6fc934ef5
Hyun Woo <hwsa10041@gmail.com>
2023-02-10 23:42:20 +0900

    [FEAT, CHORE] Board 관련 Service 추가 및 수정
    - 게시글 등록시 컬럼 값 및 return 방식변경
    - 게시글 목록 조회시 일반유저 / 가입된 유저 구분 및 좋아요 수, 좋아요 등록 여부 추가
    - 게시글 좋아요 기능 구현

--- src/main/java/callbusLab/zaritalk/Assignment/domain/board/controller/BoardController.java
+++ src/main/java/callbusLab/zaritalk/Assignment/domain/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package callbusLab.zaritalk.Assignment.domain.board.controller;
 
 import callbusLab.zaritalk.Assignment.domain.board.dto.BoardDto;
+import callbusLab.zaritalk.Assignment.domain.board.dto.LikesDto;
 import callbusLab.zaritalk.Assignment.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
     private final BoardService boardService;
 
+    // 글 생성
     @PostMapping("board")
     @PreAuthorize("hasAnyRole('LESSOR','REALTOR','LESSEE')")
     public ResponseEntity<BoardDto.CreateDto> boardAdd(
@@ -21,10 +23,19 @@ public class BoardController {
         return boardService.addBoard(request);
     }
 
+    // 글 목록 조회
     @GetMapping("board")
     public ResponseEntity<Page<BoardDto.PostsListDto>> boardList(
             @RequestParam Integer page, @RequestParam Integer limit, @RequestParam String filter, @RequestParam String arrange
     ) {
         return boardService.findListBoard(page, limit, filter, arrange);
+    }
+
+    // 좋아요
+    @PostMapping("board/like")
+    public ResponseEntity<LikesDto.addDto> likeSave(
+            @RequestBody LikesDto.addDto request
+    ) {
+        return boardService.saveLike(request);
     }
 }
--- src/main/java/callbusLab/zaritalk/Assignment/domain/board/service/BoardService.java
+++ src/main/java/callbusLab/zaritalk/Assignment/domain/board/service/BoardService.java
@@ -1,12 +1,16 @@
 package callbusLab.zaritalk.Assignment.domain.board.service;
 
 import callbusLab.zaritalk.Assignment.domain.board.dto.BoardDto;
+import callbusLab.zaritalk.Assignment.domain.board.dto.LikesDto;
 import callbusLab.zaritalk.Assignment.domain.board.entity.Board;
+import callbusLab.zaritalk.Assignment.domain.board.entity.Likes;
 import callbusLab.zaritalk.Assignment.domain.board.repository.BoardRepository;
+import callbusLab.zaritalk.Assignment.domain.board.repository.LikesRepository;
 import callbusLab.zaritalk.Assignment.domain.user.entity.User;
 import callbusLab.zaritalk.Assignment.domain.user.repository.UserRepository;
 import callbusLab.zaritalk.Assignment.global.config.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,67 +21,151 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static callbusLab.zaritalk.Assignment.global.config.exception.CustomErrorCode.INTERNAL_SERVER_ERROR;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class BoardService {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
+    private final LikesRepository likesRepository;
 
-    @Transactional
+    // method
+    private void likeAllSaveBoard(
+            Board board, Long postLikeAll
+    ) {
+        boardRepository.save(
+                Board.builder()
+                        .id(board.getId())
+                        .user(board.getUser())
+                        .title(board.getTitle())
+                        .bName(board.getBName())
+                        .note(board.getNote())
+                        .bImg(board.getBImg())
+                        .likeAll(postLikeAll)
+                        .createAt(board.getCreateAt())
+                        .updateAt(board.getUpdateAt())
+                        .build()
+        );
+    }
+
+    private Board verificationOptionalBoard(
+            LikesDto.addDto request
+    ) {
+        Optional<Board> byId = boardRepository.findById(request.getId());
+        if (!byId.isPresent()) {
+            throw new CustomException(INTERNAL_SERVER_ERROR);
+        }
+        return byId.get();
+    }
+
     private User getUserInfo() {
         Optional<User> byUser = userRepository.findByEmail(SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getName()
         );
-
         if (!byUser.isPresent()) {
             throw new CustomException(INTERNAL_SERVER_ERROR);
         }
 
-        User user = byUser.get();
-        return user;
+        return byUser.get();
     }
 
+    private Page<BoardDto.PostsListDto> PostsListDto(
+            Integer page, Integer limit, Sort.Direction asc, String filter, String memberCheck
+    ) {
+        Pageable pageWithTenElements = PageRequest.of(page - 1, limit, asc, filter);
+        Page<Board> list = boardRepository.findAll(pageWithTenElements);
+        Page<BoardDto.PostsListDto> collect;
+
+        if (memberCheck.equals("anonymousUser")) {
+            collect = list.map((Board board) ->
+                    BoardDto.PostsListDto.response(board, false));
+        } else {
+            collect = list.map(
+                    (Board board) -> BoardDto.PostsListDto.response(
+                            board,
+                            likesRepository
+                                    .existsByBoardIdAndUserId(board.getId(), getUserInfo().getId())
+                    )
+            );
+        }
+        return collect;
+
+    }
+    // Service
+
     @Transactional
-    public ResponseEntity<BoardDto.CreateDto> addBoard(BoardDto.CreateDto request) {
+    public ResponseEntity<BoardDto.CreateDto> addBoard(
+            BoardDto.CreateDto request
+    ) {
         User user = getUserInfo();
-        Board board = boardRepository.save(
-                Board.builder()
-                        .user(user)
-                        .title(request.getTitle())
-                        .user(user)
-                        .bName(user.getNickname())
-                        .note(request.getNote())
-                        .bImg(request.getBImg())
-                        .quit(false)
-                        .build()
-        );
+
         return new ResponseEntity<>(
                 BoardDto.CreateDto.response(
-                        board
+                        boardRepository.save(
+                                Board.builder()
+                                        .user(user)
+                                        .title(request.getTitle())
+                                        .bName(user.getNickname())
+                                        .note(request.getNote())
+                                        .bImg(request.getBImg())
+                                        .likeAll(0L)
+                                        .createAt(LocalDateTime.now().withNano(0))
+                                        .updateAt(LocalDateTime.now().withNano(0))
+                                        .build())
                 ), HttpStatus.CREATED
         );
     }
+    // 클라이언트가 요청한 페이지 / 페이지 안의 목록 수 / 어느 순으로 정렬할지 / 오름차순 or 내림차순
 
     @Transactional
-    public ResponseEntity<Page<BoardDto.PostsListDto>> findListBoard(Integer page, Integer limit, String filter, String arrange) {
-
+    public ResponseEntity<Page<BoardDto.PostsListDto>> findListBoard(
+            Integer page, Integer limit, String filter, String arrange
+    ) {
+        String memberCheck = SecurityContextHolder.getContext().getAuthentication().getName();
+        Page<BoardDto.PostsListDto> collect;
         if (arrange.equals("ASC")) {
-            Pageable pageWithTenElements = PageRequest.of(page - 1, limit, Sort.Direction.ASC, filter);
-            Page<Board> list = boardRepository.findAll(pageWithTenElements);
-            Page<BoardDto.PostsListDto> collect = list.map(BoardDto.PostsListDto::response);
-            return new ResponseEntity<>(collect, HttpStatus.OK);
+            collect = PostsListDto(page, limit, Sort.Direction.ASC, filter, memberCheck);
         } else {
-            Pageable pageWithTenElements = PageRequest.of(page - 1, limit, Sort.Direction.DESC, filter);
-            Page<Board> list = boardRepository.findAll(pageWithTenElements);
-            Page<BoardDto.PostsListDto> collect = list.map(BoardDto.PostsListDto::response);
-            return new ResponseEntity<>(collect, HttpStatus.OK);
+            collect = PostsListDto(page, limit, Sort.Direction.DESC, filter, memberCheck);
         }
+        return new ResponseEntity<>(collect, HttpStatus.OK);
+    }
 
+
+    @Transactional
+    public ResponseEntity<LikesDto.addDto> saveLike(
+            LikesDto.addDto request
+    ) {
+        boolean existsLike = likesRepository.existsByBoardIdAndUserId(
+                request.getId(), getUserInfo().getId());
+        Board board = verificationOptionalBoard(request);
+
+        // 좋아요가 눌려있는지 여부
+        if (!existsLike) {
+            likesRepository.save(
+                    Likes.builder()
+                            .board(board)
+                            .user(getUserInfo())
+                            .build()
+            );
+            likeAllSaveBoard(board, board.getLikeAll() + 1);
+            return new ResponseEntity<>(
+                    LikesDto.addDto.response(
+                            "ADD_LIKE_SUCCESS"
+                    ), HttpStatus.CREATED);
+        } else {
+            likesRepository.deleteByBoardIdAndUserId(request.getId(), getUserInfo().getId());
+            likeAllSaveBoard(board, board.getLikeAll() - 1);
+            return new ResponseEntity<>(
+                    LikesDto.addDto.response(
+                            "DELETE_LIKE_SUCCESS"
+                    ), HttpStatus.OK);
+        }
     }
 }
