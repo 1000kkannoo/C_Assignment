c1c8f85f2bb5449e62219c2a2e62f2c894ceb51f
Hyun Woo <hwsa10041@gmail.com>
2023-02-10 23:34:48 +0900

    [CHORE] Board 엔티티 수정
    - 불분명한 컬럼명 수정
    - EntityListeners 제거 -> localDateTime.now()로 변경
    - 어노테이션 사용 자제

--- src/main/java/callbusLab/zaritalk/Assignment/domain/board/entity/Board.java
+++ src/main/java/callbusLab/zaritalk/Assignment/domain/board/entity/Board.java
@@ -1,24 +1,23 @@
 package callbusLab.zaritalk.Assignment.domain.board.entity;
 
 import callbusLab.zaritalk.Assignment.domain.user.entity.User;
-import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.awt.print.Book;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
 @Builder
 @Entity
 @Table(name = "board")
-@EntityListeners(AuditingEntityListener.class)
+@DynamicUpdate
 public class Board {
 
     @Id
@@ -26,32 +25,33 @@ public class Board {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "u_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(name = "b_name", nullable = false)
+    @Column(name = "board_name", nullable = false)
     private String bName;
     @Column(name = "title", nullable = false)
     private String title;
     @Column(name = "note", nullable = false)
     private String note;
-    @Column(name = "b_img", nullable = true)
+    @Column(name = "board_img", nullable = true)
     private String bImg;
-    @Column(name = "quit", nullable = false)
-    private Boolean quit;
+    @Column(name = "like_all", nullable = false)
+    private Long likeAll;
 
     @Column(name = "create_at", nullable = false)
-    @CreatedDate
     @NotNull
     private LocalDateTime createAt;
 
     @Column(name = "update_at", nullable = false)
-    @LastModifiedDate
     @NotNull
     private LocalDateTime updateAt;
 
     @Column(name = "delete_at", nullable = true)
     private LocalDateTime deleteAt;
 
+
+    protected Board() {
+    }
 
 }
