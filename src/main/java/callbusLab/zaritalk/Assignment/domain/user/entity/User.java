c18f3d6609d01f8b74fc9b651daa6f3771035afb
Hyun Woo <hwsa10041@gmail.com>
2023-02-10 14:12:32 +0900

    [CHORE] User 접근 레벨 변경
    - private -> protected

--- src/main/java/callbusLab/zaritalk/Assignment/domain/user/entity/User.java
+++ src/main/java/callbusLab/zaritalk/Assignment/domain/user/entity/User.java
@@ -38,6 +38,6 @@ public class User {
             inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
     private Set<Authority> authorities;
 
-    private User() {
+    protected User() {
     }
 }
