32ffaf876225d4190c2dc637a70a118771462f48
Hyun Woo <hwsa10041@gmail.com>
2023-02-12 13:59:58 +0900

    [FEAT] 글 목록 조회시 Exception 메시지 추가

--- src/main/java/callbusLab/zaritalk/Assignment/global/config/exception/CustomErrorCode.java
+++ src/main/java/callbusLab/zaritalk/Assignment/global/config/exception/CustomErrorCode.java
@@ -12,6 +12,10 @@ public enum CustomErrorCode {
     NOT_EXISTS_EMAIL("해당 계정은 존재하지 않습니다."),
     NOT_MATCHED_ID_OR_PASSWORD("아이디 또는 비밀번호를 잘못 입력하였습니다."),
 
+    INVALID_REQUEST_ARRANGE("잘못된 정렬 방식을 요청했습니다"),
+    OVER_LIMIT("한 페이지에 16개 이상의 게시글을 요청할 수 없습니다."),
+    INVALID_REQUEST_FILTER("업데이트와 좋아요 수 로만 정렬할 수 있습니다."),
+
     // 알수 없는 오류의 처리
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
